61bc909f204b824fd6214aa5de25a774c456f849
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-24 17:18:44 +0530

    REFACTORED: ADDED AND REMOVED CODE

--- convertFeetIntoInch-test.js
+++ convertFeetIntoInch-test.js
@@ -1,39 +1,39 @@
 const assert = require("chai").assert;
-const convertFeetIntoInch = require("./convertFeetIntoInch");
+const measurement = require("./convertFeetIntoInch");
 
 describe("Test case for feet", function() {
   it("should return true for 0 feet and 0 feet", function() {
-    let isEqual = convertFeetIntoInch(0);
+    let isEqual = measurement.convertFeetIntoInch(0);
     assert.equal(isEqual, 0);
   });
   it("should return true if number is null", function() {
-    let isNull = convertFeetIntoInch();
+    let isNull = measurement.convertFeetIntoInch();
     assert.equal(isNull, true);
   });
   it("should return false if typeof number is not number", function() {
-    let isNum = convertFeetIntoInch("4");
+    let isNum = measurement.convertFeetIntoInch("4");
     assert.equal(isNum, false);
   });
 });
 
 describe("Test case for inch", function() {
   it("should return true if converted value is null", function() {
-    let isNull = convertFeetIntoInch(null);
+    let isNull = measurement.convertFeetIntoInch(null);
     assert.equal(isNull, true);
   });
   it("should return true if converted value is number", function() {
-    assert.isNumber(convertFeetIntoInch(2));
+    assert.isNumber(measurement.convertFeetIntoInch(2));
   });
   it("should return 24 if pass 2", function() {
-    let value = convertFeetIntoInch(2);
+    let value = measurement.convertFeetIntoInch(2);
     assert.equal(value, 24);
   });
   it("should not return 1 if 1ft is not eq to 1 inch", function() {
-    let value = convertFeetIntoInch(1);
+    let value = measurement.convertFeetIntoInch(1);
     assert.notEqual(value, 1);
   });
   it("should return 12inch", function() {
-    let value = convertFeetIntoInch(1);
+    let value = measurement.convertFeetIntoInch(1);
     assert.equal(value, 12);
   });
 });
--- convertFeetIntoInch.js
+++ convertFeetIntoInch.js
@@ -1,31 +1,35 @@
-/**
- * to convery feet into inch
- * @param  {} number
- */
-function convertFeetIntoInch(number) {
-  let convertedValue=0;
+function measurement() {
+  /**
+   * to convery feet into inch
+   * @param  {} number
+   */
+  function convertFeetIntoInch(number) {
+    let convertedValue = 0;
 
-  console.log("Welcome to Quantity Measurement");
+    console.log("Welcome to Quantity Measurement");
 
-  //when number is zero
-  if (number == 0) {
+    //when number is zero
+    if (number == 0) {
+      return convertedValue;
+    }
+
+    //when number is null
+    if (number == null) {
+      return true;
+    }
+
+    //number type check
+    if (typeof number != "number") {
+      return false;
+    }
+
+    //convert feet into inch
+    convertedValue = number * 12;
     return convertedValue;
   }
-
-  //when number is null
-  if (number == null) {
-    return true;
-  }
-
-  //number type check
-  if (typeof number != "number") {
-    return false;
-  }
-
-  //convert feet into inch
-  convertedValue = number * 12;
-  return convertedValue;
+  return {
+    convertFeetIntoInch
+  };
 }
 
-
-module.exports = convertFeetIntoInch;
+module.exports = measurement();
