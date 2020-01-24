c5398133fa1f12a503712f42541d6ba25265564d
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-24 12:31:31 +0530

    INITIALISED: 01basictest.js

---
+++ 01basictest.js
@@ -0,0 +1,10 @@
+let assert = require('chai').assert;
+
+describe('Basic Mocha String Test',function(){
+    it('should return number of characters in a string',function(){
+        assert.equal("Hello".length,4);
+    });
+    it('should return first character of the string',function(){
+        assert.equal("Hello".charAt(0),'H');
+    });
+});
