f6953a731ef7c5feb191f09ff877e86debf549d4
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-24 16:19:17 +0530

    ADDED: TESTCASE TO CHECK OUTPUT TYPE
    Added testcase to check converted value type is number or not.

--- convertFeetIntoInch-test.js
+++ convertFeetIntoInch-test.js
@@ -1,24 +1,31 @@
 const assert = require("chai").assert;
 const convertFeetIntoInch = require("./convertFeetIntoInch");
 
-describe("Test case for feet",function(){
-    it("should return true for 0 feet and 0 feet",function(){
-        let isEqual = convertFeetIntoInch(0);
-        assert.equal(isEqual,0);
-    });
-    it("should return true if number is null",function(){
-        let isNull = convertFeetIntoInch();
-        assert.equal(isNull,true);
-    })
-    it("should return false if typeof number is not number",function(){
-        let isNum = convertFeetIntoInch("4");
-        assert.equal(isNum,false);
-    });
+describe("Test case for feet", function() {
+  it("should return true for 0 feet and 0 feet", function() {
+    let isEqual = convertFeetIntoInch(0);
+    assert.equal(isEqual, 0);
+  });
+  it("should return true if number is null", function() {
+    let isNull = convertFeetIntoInch();
+    assert.equal(isNull, true);
+  });
+  it("should return false if typeof number is not number", function() {
+    let isNum = convertFeetIntoInch("4");
+    assert.equal(isNum, false);
+  });
 });
 
-describe("Test case for inch",function(){
-    it("should return true if converted value is null",function(){
-        let isNull = convertFeetIntoInch(null);
+describe("Test case for inch", function() {
+  it("should return true if converted value is null", function() {
+    let isNull = convertFeetIntoInch(null);
     assert.equal(isNull, true);
-    });
+  });
+  it("should return true if converted value is number", function() {
+    let isNum = convertFeetIntoInch(2);
+    if (typeof isNum === "number") {
+      isNum = true;
+    }
+    assert.equal(isNum, true);
+  });
 });
