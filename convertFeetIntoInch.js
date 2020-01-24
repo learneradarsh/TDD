faae27603a40818d3bf29a3b4749dbf72cc6ebfc
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-24 15:49:40 +0530

    ADDED: IF CONDITION TO CHECK NULL
    Added if condition to check convertedValue to inch is null or not

--- convertFeetIntoInch.js
+++ convertFeetIntoInch.js
@@ -2,20 +2,33 @@
  * to convery feet into inch
  * @param  {} number
  */
-function convertFeetIntoInch(number){
-    
-    console.log("Welcome to Quantity Measurement");
+function convertFeetIntoInch(number) {
+  let convertedValue = null;
 
-    //when number is zero    
-    if(number == 0){ return 0; }
+  console.log("Welcome to Quantity Measurement");
 
-    //when number is null
-    if(number == null) { return  true; }
+  //when number is zero
+  if (number == 0) {
+    return 0;
+  }
 
-    //number type check
-    if(typeof(number) != "number") { return false; }
+  //when number is null
+  if (number == null) {
+    return true;
+  }
 
+  //number type check
+  if (typeof number != "number") {
+    return false;
+  }
 
+  //when converted value is null
+  if (convertedValue == null) {
+    return true;
+  }
+  //convert feet into inch
+  convertedValue = number * 12;
+  return convertedValue;
 }
 
 module.exports = convertFeetIntoInch;
