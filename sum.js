b319d2e70747ce9a1054cbdc82a4777ca8dd72c1
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-24 11:56:52 +0530

    ADDED: SUM FUNCTION
    Added sum(a,b) to add a and b.

--- sum.js
+++ sum.js
@@ -3,6 +3,26 @@
  * @param  {} a
  * @param  {} b
  */
-function sum(a,b){
+function sum(a, b) {
+  //Case 1: Check the function for no arguments
+  if (a == "" || b == "") {
+    return 0;
+  }
 
+  //Case 2: Check the function for partial arguments
+  if (typeof a === undefined || typeof b === undefined) {
+    return 0;
+  }
+
+  //Case 3: Check the function for valid integer arguments
+  if (isNaN(a) || isNaN(b)) {
+    return 0;
+  }
+
+  //Case 4: Check the function for sum of 2 valid integers
+  let c = a + b;
+  return c;
 }
+
+//Export the function
+module.exports = sum;
