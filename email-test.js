7b349ba90fc26ed5874a378df79f41d9f30cf6fd
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-25 18:07:44 +0530

    REFACTORED

--- email-test.js
+++ email-test.js
@@ -1,9 +1,43 @@
 const assert = require("chai").assert;
 const isValidEmail = require("./email");
+let isContains;
 
-describe("test case to check email",function(){
-    it("should return false if email doesn't contains @",function(){
-        let isContains = isValidEmail("abc");
-        assert.equal(isContains,false);
-    });
+describe("test case to check email", function() {
+  it("should return error if email doesn't contains @", function() {
+    isContains = isValidEmail("abc");
+    assert.equal(isContains, "must contains “@” symbol");
+  });
+  it("should return error if tld starts with '.'", function() {
+    isContains = isValidEmail("abc@.com.my");
+    assert.equal(isContains,"tld can not start with dot “.”");
+  });
+  it("should return error if tld contains only one character",function(){
+      isContains = isValidEmail("abc123@gmail.a");
+      assert.equal(isContains,"last tld must contains at least two characters");
+  });
+  it("should return error if tld starts with dot",function(){
+       isContains = isValidEmail("abc123@.com");
+       assert.equal(isContains,"tld can not start with dot “.”");
+
+  });
+  it("should return error if tld starts with dot",function(){
+    isContains = isValidEmail("abc123@.com.com");
+    assert.equal(isContains,"tld can not start with dot “.”");
+  });
+  it("should return error if first character of email starts with dot",function(){
+      isContains = isValidEmail(".abc@abc.com");
+      assert.equal(isContains,"email’s 1st character can not start with “.”");
+  });
+  it("should return error if email is containing character,digit, underscore, and dash",function(){
+      isContains = isValidEmail("abc()*@gmail.com");
+      assert.equal(isContains,"email’s is only allow character, digit, underscore and dash");
+  });
+  it("should return error if tld contains any special chars",function(){
+      isContains = isValidEmail("abc@%*.com");
+      assert.equal(isContains,"email’s tld is only allow character and digit");
+  });
+  it("should return error if email contains double dot",function(){
+    isContains = isValidEmail("abc..2002@gmail.com");
+    assert.equal(isContains,"double dots “.” are not allow");
+  });
 });
--- email.js
+++ email.js
@@ -6,15 +6,22 @@ function isValidEmail(email) {
   const pattern = {
     emailReg: /^(^[^.][\w-]|(?<!\.)\.)+[a-zA-Z0-9]@[a-zA-Z0-9]([\w\-]+)((\.([a-zA-Z]){2,9})+)$/,
     errorAtRateReg: /(?=.*[@])/,
-    tldDotReg: /^([\w-]|(?<!\.)\.)+[a-zA-Z0-9]@[a-zA-Z0-9]([\w\-]+)((\.([a-zA-Z]){0,9})+)$/,
-    tldTwoCharReg: /^([\w-]|(?<!\.)\.)+[a-zA-Z0-9]@[a-zA-Z0-9]([\w\-]+)((\.([a-zA-Z]){2,9})+)$/,
-    canNotDotReg: /^[^.]/
+    tldDotReg: /@[a-zA-Z0-9%*()&!<>{}]([a-zA-Z0-9%*()&!<>{}]+)((\.([a-zA-Z]){0,9})+)$/,
+    tldTwoCharReg: /@[a-zA-Z0-9%*()&!<>{}]([a-zA-Z0-9%*()&!<>{}]+)((\.([a-zA-Z]){2,9})+)$/,
+    canNotDotReg: /^[^.]/,
+    canNotSpecialReg: /^([\w-]|(?<!\.)\.)+[a-zA-Z0-9]@[a-zA-Z0-9%*()&!<>{}]([%*()&!<>{}]+)((\.([a-zA-Z]){2,9})+)$/,
+    onlyCharDigReg: /([\w-]|(?<!\.)\.)+[a-zA-Z0-9]@[a-zA-Z0-9]([\w\-]+)((\.([a-zA-Z]){2,9})+)$/,
+    dblDotsReg: /([\w-]|(?<!\.)\.)+[a-zA-Z0-9]@[a-zA-Z0-9]([\w\-]+)((\.([a-zA-Z]){2,9})+)$/
   };
   const errorMsgs = {
     errorAtRate: "must contains “@” symbol",
     tldDot: "tld can not start with dot “.”",
     tldTwoChar: "last tld must contains at least two characters",
-    canNotDot: "email’s 1st character can not start with “.”"
+    canNotDot: "email’s 1st character can not start with “.”",
+    canNotSpecial:
+      "email’s is only allow character, digit, underscore and dash",
+    onlyCharDig: "email’s tld is only allow character and digit",
+    dblDots: "double dots “.” are not allow"
   };
 
   if (pattern.emailReg.test(email)) {
@@ -25,10 +32,16 @@ function isValidEmail(email) {
     return errorMsgs.tldDot;
   } else if (!pattern.tldTwoCharReg.test(email)) {
     return errorMsgs.tldTwoChar;
-  }else if(!pattern.canNotDotReg.test(email)){ //till 6th
+  } else if (!pattern.canNotDotReg.test(email)) {
     return errorMsgs.canNotDot;
+  } else if (!pattern.canNotSpecialReg.test(email)) {
+    return errorMsgs.canNotSpecial;
+  } else if (!pattern.onlyCharDigReg.test(email)) {
+    return errorMsgs.onlyCharDig;
+  } else if (!pattern.dblDotsReg.test(email)) {
+    return errorMsgs.dblDots;
   }
 }
 
-console.log(isValidEmail("abc()*@gmail.com"));
+// console.log(isValidEmail("abc@.com.my"));
 module.exports = isValidEmail;
