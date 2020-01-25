b14ab2d7c9c1620c539b67e0f4d6d4a783e06b83
learneradarsh <learner.adarsh@gmail.com>
2020-01-25 22:09:58 +0530

    ADDED: TESTCASE TO CHECK generateInvoice return object

--- cabInvoiceGen-test.js
+++ cabInvoiceGen-test.js
@@ -3,59 +3,59 @@ const CabInvoiceGen = require("./cabInvoiceGen");
 
 const cabInvoiceGen = new CabInvoiceGen();
 
-describe("test cases for calcCabFare agrument values validations", function() {
-  it("should return true if calCabFare has no arguments", function() {
+describe("test cases for calcCabFare agrument values validations", function () {
+  it("should return true if calCabFare has no arguments", function () {
     let isNull = cabInvoiceGen.calcCabFare();
     assert.equal(isNull, true);
   });
-  it("should return true if calcCabFare has partial agrs", function() {
+  it("should return true if calcCabFare has partial agrs", function () {
     let isPartial = cabInvoiceGen.calcCabFare(2);
     assert.equal(isPartial, true);
   });
-  it("should not equal if calcCabFare has all args", function() {
+  it("should not equal if calcCabFare has all args", function () {
     let isAllArgs = cabInvoiceGen.calcCabFare(2, 4);
     assert.notEqual(isAllArgs, true);
   });
-  it("should return true if calcCabFare has invalid type args", function() {
+  it("should return true if calcCabFare has invalid type args", function () {
     let isValid = cabInvoiceGen.calcCabFare("a", "b");
     assert.equal(isValid, true);
   });
-  it("should not equal if calcCabFare has valid type args", function() {
+  it("should not equal if calcCabFare has valid type args", function () {
     let isValid = cabInvoiceGen.calcCabFare(2, 4);
     assert.notEqual(isValid, true);
   });
 });
 
-describe("test cases for output of calcCabFare", function() {
-  it("should return 5 if distance is zero", function() {
+describe("test cases for output of calcCabFare", function () {
+  it("should return 5 if distance is zero", function () {
     let fare = cabInvoiceGen.calcCabFare(0, 0);
     assert.equal(fare, 5);
   });
-  it("should return value if calcCabFare output is not null", function() {
+  it("should return value if calcCabFare output is not null", function () {
     let isNull = cabInvoiceGen.calcCabFare(5, 5);
     assert.notEqual(isNull, null);
   });
-  it("should return 140 for distance=2km and time=2hrs", function() {
+  it("should return 140 for distance=2km and time=2hrs", function () {
     let fare = cabInvoiceGen.calcCabFare(2, 2);
     assert.equal(fare, 140);
   });
 });
 
-describe("test cases for multipleRideFare", function() {
-  it("should return true if multipleRideFare has no arg", function() {
+describe("test cases for multipleRideFare", function () {
+  it("should return true if multipleRideFare has no arg", function () {
     let isNull = cabInvoiceGen.multipleRideFare();
     assert.equal(isNull, true);
   });
-  it("should return true if multipleRideFare has invalid type arg", function() {
+  it("should return true if multipleRideFare has invalid type arg", function () {
     let isValid = cabInvoiceGen.multipleRideFare("23");
     assert.equal(isValid, true);
   });
-  it("should return 0 for 0 rides", function() {
+  it("should return 0 for 0 rides", function () {
     let rides = [];
     let totalRides = cabInvoiceGen.multipleRideFare(rides);
     assert.equal(totalRides, 0);
   });
-  it("should not equal to null if agrTotalFare is not null", function() {
+  it("should not equal to null if agrTotalFare is not null", function () {
     let rides = [
       {
         distance: 4,
@@ -65,7 +65,7 @@ describe("test cases for multipleRideFare", function() {
     let isNull = cabInvoiceGen.multipleRideFare(rides);
     assert.notEqual(isNull, null);
   });
-  it("should return 630 if calculated correct output", function() {
+  it("should return 630 if calculated correct output", function () {
     let rides = [
       {
         distance: 4,
@@ -79,7 +79,7 @@ describe("test cases for multipleRideFare", function() {
     let agrFare = cabInvoiceGen.multipleRideFare(rides);
     assert.equal(agrFare, 630);
   });
-  it("should return 10 if minimum fare condition is working for 2 rides where total distance and time is zero", function() {
+  it("should return 10 if minimum fare condition is working for 2 rides where total distance and time is zero", function () {
     let rides = [
       {
         distance: 0,
@@ -95,13 +95,26 @@ describe("test cases for multipleRideFare", function() {
   });
 });
 
-describe("test cases for generateInvoice", function() {
-  it("should return true if generateInvoice has no arg", function() {
-    let isNull = cabInvoiceGen.multipleRideFare();
+describe("test cases for generateInvoice", function () {
+  it("should return true if generateInvoice has no arg", function () {
+    let isNull = cabInvoiceGen.generateInvoice();
     assert.equal(isNull, true);
   });
-  it("should return true if generateInvoice has invalid type arg", function() {
-    let isValid = cabInvoiceGen.multipleRideFare("23");
+  it("should return true if generateInvoice has invalid type arg", function () {
+    let isValid = cabInvoiceGen.generateInvoice("23");
     assert.equal(isValid, true);
+  });
+  it("should return totalRides 2, totalFare 2, and avgFarePerRide 1", function () {
+    let rides = [{
+      percentage: 4,
+      time: 4
+    }, {
+      percentage: 5,
+      time: 5
+    }];
+    let invoice = cabInvoiceGen.generateInvoice(rides);
+    assert.equal(invoice.totalFare, 2);
+    assert.equal(invoice.totalRides, 2);
+    assert.equal(invoice.avgFarePerRide, 1);
   });
 });
