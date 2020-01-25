27e18597d09de4626d7bc297bcdede4bedb05271
Adarsh Learner <learner.adarsh@gmail.com>
2020-01-25 13:57:41 +0530

    ADDED: convertFeetIntoYard(number)
    Added function to convert feet into yard

--- convertFeetIntoInch.js
+++ convertFeetIntoInch.js
@@ -27,8 +27,37 @@ function measurement() {
     convertedValue = number * 12;
     return convertedValue;
   }
+
+  
+  /**
+   * to convert feet into yard
+   * @param  {} number
+   */
+  function convertFeetIntoYard(number) {
+    let convertedValue = 0;
+
+    //when number is zero
+    if (number == 0) {
+      return convertedValue;
+    }
+
+    //when number is null
+    if (number == null) {
+      return true;
+    }
+
+    //number type check
+    if (typeof number != "number") {
+      return false;
+    }
+
+    convertedValue = number * (1/3);
+    return convertedValue;
+
+  }
   return {
-    convertFeetIntoInch
+    convertFeetIntoInch,
+    convertFeetIntoYard
   };
 }
 
